b1d4ce39b60443c495e78817a9341c9030ab6e5e
Redando Dieujuste <101023464+9308RD00@users.noreply.github.com>
2024-10-29 13:19:21 +0000

    new4

---
+++ src/js/component/Square.jsx
@@ -0,0 +1,4 @@
+import react from "react";
+import "./style.css";
+import Home from "./home";
+
--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,108 @@
-import React from "react";
-
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import React, { useState } from "react";
+import "./style.css";
 
 //create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+  const [squareValues, setSquareValues] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
+  const [currPlayer, setCurrPlayer] = useState("X");
+  const winningCombinations = [
+    //horizontal straight lines
+    [1, 1, 1, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 1, 1, 1, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 1, 1, 1],
+    //vertical straight lines
+    [1, 0, 0, 1, 0, 0, 1, 0, 0],
+    [0, 1, 0, 0, 1, 0, 0, 1, 0],
+    [0, 0, 1, 0, 0, 1, 0, 0, 1],
+    //diagonal straight lines
+    [1, 0, 0, 0, 1, 0, 0, 0, 1],
+    [0, 0, 1, 0, 1, 0, 1, 0, 0],
+  ];
+
+  const checkWinner = (updatedArray) => {
+    winningCombinations.forEach((comboArray) => {
+      var count = 0;
+      comboArray.forEach((value, ind) => {
+        if (value == 1) {
+          if (updatedArray[ind] == currPlayer) {
+            count++;
+            console.log(count);
+
+            if (count >= 3) {
+              alert("Winner is " + currPlayer);
+            }
+          }
+        }
+      });
+    });
+  };
+
+  const currentMove = (index) => {
+    let newValues = [...squareValues];
+
+    if (newValues[index] == "") {
+      newValues[index] = currPlayer;
+      setSquareValues(newValues);
+
+      checkWinner(newValues);
+
+      setCurrPlayer(currPlayer == "X" ? "O" : "X");
+    }
+  };
+
+  return (
+    <div className="App">
+      <h1 style={{ color: "blue" }}>TicTacToe</h1>
+      <div className="row">
+        <div onClick={() => currentMove(0)} className="col-4">
+          {squareValues[0]}
+        </div>
+        <div onClick={() => currentMove(1)} className="col-4">
+          {squareValues[1]}
+        </div>
+        <div onClick={() => currentMove(2)} className="col-4">
+          {squareValues[2]}
+        </div>
+      </div>
+      <div className="row">
+        <div onClick={() => currentMove(3)} className="col-4">
+          {squareValues[3]}
+        </div>
+        <div onClick={() => currentMove(4)} className="col-4">
+          {squareValues[4]}
+        </div>
+        <div onClick={() => currentMove(5)} className="col-4">
+          {squareValues[5]}
+        </div>
+      </div>
+      <div className="row">
+        <div onClick={() => currentMove(6)} className="col-4">
+          {squareValues[6]}
+        </div>
+        <div onClick={() => currentMove(7)} className="col-4">
+          {squareValues[7]}
+        </div>
+        <div onClick={() => currentMove(8)} className="col-4">
+          {squareValues[8]}
+        </div>
+      </div>
+      <button
+       type="reset" style={{ width: "100px", marginTop: "5px", borderRadius: "10px" }}
+      >
+        Reset
+      </button>
+    </div>
+  );
 };
 
 export default Home;
